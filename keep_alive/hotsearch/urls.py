457962f5f74c5b2324e472f7edef829c50ac56a1
June Drinleng <946788898@qq.com>
2025-06-06 19:53:26 +0800

    remove

--- keep_alive/hotsearch/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # 访问 / 时，调用 views.home
-]
--- keep_alive/keepalive_django/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('hotsearch.urls')),  # 把根路径交给 hotsearch 应用处理
-]
